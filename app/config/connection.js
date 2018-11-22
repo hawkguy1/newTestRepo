0041441a646bef499a0cf85f8ffd4808eeb78dcc
Andy <andy.harringa@gmail.com>
2018-11-21 17:26:09 -0700

    added references

---
+++ app/config/connection.js
@@ -0,0 +1,28 @@
+// ----- SETS UP MYSQL CONNECTION ----- //
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "seat_yourself"
+});
+var connection2 = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "table_reservations"
+});
+
+// ----- MAKES CONNECTION ----- //
+connection.connect(function(err) {
+  if (err) {
+    console.error("ERROR CONNECTING: " + err.stack);
+    return;
+  }
+  console.log("CONNECTED AS ID: " + connection.threadId);
+});
+
+// ----- EXPORTS CONNECTION ----- //
+module.exports = connection;
